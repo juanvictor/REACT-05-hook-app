dedcff7fdd662ea401603edb744f167c5d8d5dc0
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-10-12 00:22:55 -0400

    01-useState uso de este hook

---
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+import { useCouter } from '../../hooks/useCounter';
+
+
+import './counter.css';
+
+
+export const CounterWithCustomHook = () => {
+
+    const { state, increment, decrement, reset } =useCouter(100);
+
+    return (
+        <>
+            <h1>Counter with Hook: { state }</h1>
+
+            <button onClick={ () => increment(2) } className="btn"> + 1 </button>
+            <button onClick={ reset } className="btn"> Reset </button>
+            <button onClick={ () => decrement(2) } className="btn"> -1 </button>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
-    <HookApp />,
+    <CounterWithCustomHook />,
     document.getElementById('root')
 );
